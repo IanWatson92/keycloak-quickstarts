a7acb6a7ab5e22d1273c664c2b17f7d808c88c04
Ian Watson <iwatson@iwatson1.remove.csb>
2020-03-23 14:51:13 +0000

    adding appjs

--- app-jee-html5/src/main/webapp/app.js
+++ app-jee-html5/src/main/webapp/app.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 var keycloak = new Keycloak();
-var serviceUrl = 'http://127.0.0.1:8080/service'
+var serviceUrl = 'http://nodejs-keycloak-nodejs.ianwatson-8e403d02da27f23cda259248b817e83d-0001.eu-gb.containers.appdomain.cloud/service'
 
 function notAuthenticated() {
     document.getElementById('not-authenticated').style.display = 'block';
